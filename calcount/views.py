8318fae0fdd96caa9d0e43eb023146bf3b44a736
Your Name <you@example.com>
2025-03-02 15:01:02 +0545

    final

--- calcount/views.py
+++ calcount/views.py
@@ -1,22 +1,16 @@
 from django.shortcuts import render
 
-#GXrjHsmwOFZbQDOdN0uoLg==S9NfWPu2F3y2KHub
+
 # Create your views here.
 def home(request):
     import requests
     import json
-    # query = '1lb brisket and fries'
-    # api_url = 'https://api.api-ninjas.com/v1/nutrition?query={}'.format(query)
-    # response = requests.get(api_url, headers={'X-Api-Key': 'GXrjHsmwOFZbQDOdN0uoLg==S9NfWPu2F3y2KHub'})
-    # if response.status_code == requests.codes.ok:
-    #     print(response.text)
-    # else:
-    #     print("Error:", response.status_code, response.text)
+
 
     if request.method == 'POST':
         query = request.POST['query']
         api_url = 'https://api.api-ninjas.com/v1/nutrition?query='
-        api_request = requests.get(api_url + query, headers = {'X-Api-Key': 'GXrjHsmwOFZbQDOdN0uoLg==S9NfWPu2F3y2KHub'})
+        api_request = requests.get(api_url + query, headers = {'X-Api-Key': 'Enter your API key'})
         try:
             api = json.loads(api_request.content)
             print(api_request.content)
